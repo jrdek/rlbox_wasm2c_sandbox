d2b6f95155c5a16af31abae7356bd854687455e7
Joey Rudek <rudek.joseph@gmail.com>
2022-05-17 16:31:46 -0700

    shell script which works but is messy; TODO fix

--- makeWasmLib.sh
+++ makeWasmLib.sh
@@ -11,16 +11,16 @@ CFILENAME=$(basename $1 .c)
 
 # Use wasi-clang to combine library sources and wasm2c_sandbox_wrapper.c
 # into a wasm module
-SB_WRAPPER=c_src/wasm2c_sandbox_wrapper.c
+#SB_WRAPPER=c_src/wasm2c_sandbox_wrapper.c
 WASI_SYSROOT=build/_deps/wasiclang-src/share/wasi-sysroot
-LDFLAGS="-Wl,--export-all -Wl,--no-entry -Wl,--growable-table"
+LDFLAGS="-Wl,--export-all -Wl,--growable-table"
 WASI_CLANG=build/_deps/wasiclang-src/bin/clang
-$WASI_CLANG --sysroot $WASI_SYSROOT $LDFLAGS $SB_WRAPPER $1 -o $ROOT/test/lib$CFILENAME.wasm
+$WASI_CLANG --sysroot $WASI_SYSROOT $LDFLAGS $1 -o $ROOT/test/lib$CFILENAME.wasm
 
 # use modified wasm2c to turn the generated .wasm file into a .c and .h
 build/_deps/mod_wasm2c-src/bin/wasm2c -o $ROOT/test/libWasm$CFILENAME.c $ROOT/test/lib$CFILENAME.wasm
 
 # finally, compile the .c and .h along with the wasm runtime into a shared library
-gcc -shared -fPIC -O3 -o $ROOT/test/libWasm$CFILENAME.so $ROOT/test/libWasm$CFILENAME.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-impl.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-os-unix.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-os-win.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-wasi.c
+gcc -shared -fPIC -O3 -o $ROOT/test/libWasm$CFILENAME.so $ROOT/test/libWasm$CFILENAME.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-os-unix.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-os-win.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-wasi.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt-impl.c build/_deps/mod_wasm2c-src/wasm2c/wasm-rt.h -I build/_deps/mod_wasm2c-src/wasm2c
 
 cd -
